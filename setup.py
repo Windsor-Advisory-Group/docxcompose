3d866a27d3c98e2d5c86416fd9eb0fc4db9fa9c7
BCompertoreWAG <82907031+BCompertoreWAG@users.noreply.github.com>
2026-01-23 09:08:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 # -*- coding: utf-8 -*-
 """Installer for the docxcompose package."""
 
-from setuptools import find_packages
-from setuptools import setup
-
+from setuptools import find_packages, setup
 
 tests_require = [
-    'pytest',
+    "pytest",
 ]
 
 setup(
-    name='docxcompose',
-    version='1.4.1.dev0',
+    name="docxcompose",
+    version="1.4.1.dev0",
     description="Compose .docx documents",
-    long_description=(open("README.rst").read() + "\n" +
-                      open("HISTORY.txt").read()),
+    long_description=(open("README.rst").read() + "\n" + open("HISTORY.txt").read()),
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Programming Language :: Python",
@@ -23,28 +20,23 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
     ],
-    keywords='Python DOCX Word OOXML',
-    author='Thomas Buchberger',
-    author_email='t.buchberger@4teamwork.ch',
-    url='https://github.com/4teamwork/docxcompose',
-    license='MIT license',
-    packages=find_packages(exclude=['ez_setup']),
+    keywords="Python DOCX Word OOXML",
+    author="Thomas Buchberger",
+    author_email="t.buchberger@4teamwork.ch",
+    url="https://github.com/4teamwork/docxcompose",
+    license="MIT license",
+    packages=find_packages(exclude=["ez_setup"]),
     include_package_data=True,
     zip_safe=True,
     install_requires=[
-        'lxml',
-        'python-docx >= 0.8.8',
-        'setuptools',
-        'six',
-        'babel',
+        "lxml",
+        "python-docx >= 0.8.8",
+        "six",
+        "babel",
     ],
     extras_require={
-        'test': tests_require,
-        'tests': tests_require,
+        "test": tests_require,
+        "tests": tests_require,
     },
-    entry_points={
-        'console_scripts': [
-            'docxcompose = docxcompose.command:main'
-        ]
-    },
+    entry_points={"console_scripts": ["docxcompose = docxcompose.command:main"]},
 )
